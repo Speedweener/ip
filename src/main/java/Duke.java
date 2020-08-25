7c3b214b6749029ddb3d07dc3db5beb68c1a400a
Kwek Zhan Hao <e0426161.u.nus.edu>
2020-08-25 20:56:31 +0800

    Level-1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -13,6 +15,20 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+
+        while(!line.equals("bye")){
+            System.out.println("____________________________________________________________");
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+            line = in.nextLine();
+        }
+
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
