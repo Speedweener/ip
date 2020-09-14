169ad6c1a42d744e2206f5fdc46770df0b78b17b
Kwek Zhan Hao <e0426161.u.nus.edu>
2020-09-14 21:56:37 +0800

    A-Collections

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,6 +6,8 @@ import duke.tasks.Deadline;
 import duke.tasks.Event;
 import duke.tasks.Task;
 import duke.tasks.Todo;
+import java.util.ArrayList;
+
 
 import java.util.Scanner;
 
@@ -19,7 +21,7 @@ public class Duke {
 
     private static int charIndex;
     private static int taskCount = 0;
-    private static Task[] tasks = new Task[100];
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -108,7 +110,7 @@ public class Duke {
                 return;
             }
         }
-        tasks[taskCount] = new Event(details, dateTime);
+        tasks.add(new Event(details, dateTime));
         taskCount++;
         addText();
     }
@@ -129,11 +131,11 @@ public class Duke {
                 return;
             }
         }
-        tasks[taskCount] = new Deadline(details, dateTime);
+        tasks.add(new Deadline(details, dateTime));
         taskCount++;
         addText();
     }
-    public static void printHelp(){
+    public static void printHelp() {
         System.out.println("Available commands are:");
         System.out.println("1) list");
         System.out.println("2) todo");
@@ -143,12 +145,12 @@ public class Duke {
         System.out.println("6) bye");
     }
 
-    public static void list(){
+    public static void list() {
         System.out.println(lineSpace);
         System.out.println("Here are the tasks in your list:");
         for(int i=0; i<taskCount; i++) {
             System.out.print((i + 1) + ".");
-            tasks[i].printTask();
+            tasks.get(i).printTask();
         }
         if(taskCount==0){
             System.out.println("Empty List");
@@ -159,7 +161,7 @@ public class Duke {
 
 
     private static void todo() {
-        tasks[taskCount] = new Todo(details);
+        tasks.add(new Todo(details));
         taskCount++;
         addText();
     }
@@ -172,10 +174,10 @@ public class Duke {
         int taskNumber = Integer.parseInt(details);
 
         if(taskNumber<=taskCount && !(taskNumber<0)){
-            if(tasks[taskNumber-1].markAsDone()) { // Returns true if task has not been marked before
+            if(tasks.get(taskNumber-1).markAsDone()) { // Returns true if task has not been marked before
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.print("  ");
-                tasks[taskNumber - 1].printTask();
+                tasks.get(taskNumber-1).printTask();
             } else {
                 System.out.println("Task has been marked as done already!");
             }
@@ -186,16 +188,16 @@ public class Duke {
     }
 
 
-    private static void addText(){ // Text to be printed when adding a new task
+    private static void addText() { // Text to be printed when adding a new task
         System.out.println(lineSpace);
         System.out.println("Got it. I've added this task:");
         System.out.print("  ");
-        tasks[taskCount-1].printTask();
+        tasks.get(taskCount-1).printTask();
         System.out.println("Now you have " + taskCount + (taskCount<=1? " task ": " tasks ") +"in the list.");
         System.out.println(lineSpace);
     }
 
-    private static void introText(){
+    private static void introText() {
 
     }
 }
